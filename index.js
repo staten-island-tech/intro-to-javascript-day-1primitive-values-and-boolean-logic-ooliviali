5d06ce86b54aa777a84a2859b2dcc9a0e0b945a0
ooliviali <57498900+ooliviali@users.noreply.github.com>
2020-03-24 14:50:11 -0400

    intro

---
+++ index.js
@@ -0,0 +1,61 @@
+console.log("Connected");
+alert ("Hello");
+
+//strings   
+/* let string = "string";
+
+let sentence = "Hello " + "Vicky"
+console.log(sentence);
+let sentence2 = " and also Angel";
+const combine = sentence + sentence2;
+console.log(combine);
+
+let student1 = "Sandra";
+let student2 = "Kelly";
+let courseName = "AP CSP";
+let teacherName = "Whalen";
+
+
+let longSentence = `${student1} and ${student2} will 
+be taking ${courseName} with ${teacherName} next year`; //template literal
+
+console.log(longSentence);
+
+////////////////////////////////////
+//numbers integers
+let year = 2020;
+let nextYear =2020 + 1;
+let past = year/ 2;
+let lastYear = year - 1;
+let farFuture = year * 100;
+
+console.log(year, nextYear, past, lastYear, farFuture);
+
+
+////////////////////////////////////
+//booleans, null, undefined
+let trueStatement = true;
+let flaseStatement = false;
+
+let undefinedVar;
+console.log(undefinedVar); //console log variables
+console.log(nonExistent); //null
+
+*/
+//if/ else if statements
+//let student1 = "Karen";
+//let age=17;
+//console.log(student === "Karen"); //true
+//console.log(student === "Karen"); //false
+//console.log(student == "karen"); //false
+//console.log(age === "17"); //false
+//console.log(age == "17"); //true
+
+let school= "middle";
+if (school == "elementary") {
+    console.log("you are in elementary school");
+} else if (school == "middle") {
+    console.log("you are in middle school");
+} else {
+    console.log("you are in high school");
+}
