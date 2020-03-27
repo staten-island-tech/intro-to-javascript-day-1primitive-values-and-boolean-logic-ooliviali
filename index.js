725b62cdbcf1ebe203e96a9549aa38558992ed5d
ooliviali <57498900+ooliviali@users.noreply.github.com>
2020-03-27 14:50:01 -0400

    lesson 2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-console.log("Connected");
-alert ("Hello");
+//console.log("Connected");
+
+//DAY 1
+/* alert ("Hello");
 
 //strings   
-/* let string = "string";
+ let string = "string";
 
 let sentence = "Hello " + "Vicky"
 console.log(sentence);
@@ -60,6 +62,8 @@ if (school == "elementary") {
     console.log("you are in high school");
 }
 
+////////////////////////////////////
+
 //stop light
 let light= ``;
 if (light === "green") {
@@ -82,7 +86,7 @@ if(kristyInt === true && baobaoInt == true) {
 } else  {
     console.log("They will get milk");
 }
-*/
+
 
 //cupcake temp
 let temp = 3465456;
@@ -99,5 +103,131 @@ else if (temp >= 73 && temp <= 76) {
 else {
     console.log("too hot");
 }
+*/
+
+////////////////////////////////////
+
+//DAY TWO
+//functions in JS
+/* function test() {  //functions are input output machines
+    console.log("I am a function");
+}
+test(); */
+/* function argument(argument) { //input
+    console.log(argument); //output
+}
+//argument("Edis");
+function add(x, y) {
+    return x + y;   //return ends function
+}
+//console.log(add(2, 7));
+let z = function add(x, y) {
+    return x + y;
+};
+z(1, 11);
+console.log(z(1,11)); */
+
+////////////////////////////////////
+
+//Scoping
+//let x = "x";   //can be redefined
+//const z = "z"; //cannot redefine "const"
+/* const hello = function() {
+    alert("hello");
+};
+const bye = function() {
+    alert("bye");
+};
+
+function beatles() {
+    hello();
+    bye();
+}
+beatles(); */
+
+//global or window scope
+/* function makeFunc() {
+    //function scope
+    const name = "Sun";
+    console.log(name);
+}
+function hello() {
+    const name = "Jaiyu";
+    console.log(name);
+}
+hello();
+makeFunc(); */
+
+/* function makeFunc() {
+    //function scope
+    const name = "Sun";
+    function displayName(name) {
+        alert(name); //inner function still has access to outer variable name
+    }
+    return displayName;
+} */
+
+////////////////////////////////////
+
+//Arrays (lists)
+/* const names = ["Michelle", "Edis", "Richard", "Vincent", "Gisele"];
+//names.push("Katrina"); 
+const Gisele = names.slice(-1); */
+
+
+////////////////////////////////////
+
+//Arrow functions and methods
+/* const names = ["Michelle", "Edis", "Richard", "Vincent", "Gisele"];
+
+names.forEach(name => console.log(name));  */ //performs function in every element in array; name=element 
+//arrow function 1)always returned     2)this is changed
+
+////////////////////////////////////
+//spread operator
+/* const numbers = [1, 2, 3];
+const add = function(x, y, z) {
+    return x + y + z;
+};
+console.log(add(...numbers)); */
+/* const numbers = [1, 50, 34, 75, 11];
+const biggestNum = Math.max(...numbers);
+console.log(biggestNum); */
+
+////////////////////////////////////
+//challenge 1
+const students = [
+    "Riya",
+    "Omor", 
+    "Alan", 
+    "Jaiyu", 
+    "Michael", 
+    "Lisa",
+    "Richard"
+];
+
+const filter = students.filter(student => student.length < 5);
+console.log(filter);
+
+//challenge 2
+
+const students2 = [
+    "Riya", 
+    "Omor", 
+    "Alan", 
+    "Jaiyu", 
+    "Michael", 
+    "Lisa",
+    "Richard"
+];
+
+//includes junshen? false
+console.log(students2.includes("junshen"));
+
+//includes alan or savva? true
+console.log(students2.includes("Alan" || "Savva"));
+
+
+
 
 
